3f3ac82ad320b85ba16dcc2027f76585be1f218f
Isaac <uniqucode@gmail.com>
2022-08-24 10:55:46 +0100

    Refrencing progress container

--- js/script.js
+++ js/script.js
@@ -2,3 +2,4 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
