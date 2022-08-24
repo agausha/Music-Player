7fbb09962ac9c05eea511f7915073b7dc5f4bd91
Isaac <uniqucode@gmail.com>
2022-08-24 10:58:58 +0100

    Refrencing duration

--- js/script.js
+++ js/script.js
@@ -5,3 +5,4 @@ const music = document.querySelector('audio');
 const progressContainer = document.getElementById('progress-container');
 const progress = document.getElementById('progress');
 const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
